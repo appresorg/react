7e56b2933b519e707d7b749648c8d5f07da0a0a0
APPRES <support@appres.org>
2021-03-09 16:32:27 +0900

    appres html process

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="appres" appres="html">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
